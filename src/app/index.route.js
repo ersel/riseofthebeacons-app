ba53d931b5f6b4e92d5218d9b41cd87eea9342b2
Spyros Stylianou <spyros.stylianou-2@student.manchester.ac.uk>
2015-10-24 15:02:51 +0100

    Added Leaderboard route and view

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,12 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('leaderboard', {
+        url: '/leaderboard',
+        templateUrl: 'app/leaderboard/leaderboard.html',
+        controller: 'LeaderboardController',
+        controllerAs: 'leaderboard'
       });
 
     $urlRouterProvider.otherwise('/');
