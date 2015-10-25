665008baa4ec8c36c9da2f52f73ae9c77249d4c7
Spyros Stylianou <spyros.stylianou-2@student.manchester.ac.uk>
2015-10-25 10:31:20 +0000

    no message

--- src/app/barchart/barchart.controller.js
+++ src/app/barchart/barchart.controller.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module("hackMcr").controller("BarChartController", function ($scope) {
-      $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+      $scope.names = ['Bob', 'Tony', 'Greg', 'Lucy', 'Peppa', 'Alicia', 'Rick'];
 
       $scope.data = [
         [65, 59, 80, 81, 56, 55, 40]
